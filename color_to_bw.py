fb67015ea4338e8c4300f325adc9fa2049c3360e
Mitchell Hwang <mitchhwang1418@gmail.com>
2018-12-09 11:02:34 -0500

    Clean color_to_bw

--- color_to_bw.py
+++ color_to_bw.py
@@ -2,15 +2,14 @@ import cv2
 
 def color_to_bw(img):
     try:
-        image = cv2.imread(img)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        return gray
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        return image
     except Exception as e:
-        print('Error')
+        print(e)
         return str(e)
 
 if __name__ == "__main__":
-    img = 'the_dunk.jpeg'
+    img = cv2.imread('the_dunk.jpeg')
     bw = color_to_bw(img)
     cv2.imshow('Gray image', bw)
     
